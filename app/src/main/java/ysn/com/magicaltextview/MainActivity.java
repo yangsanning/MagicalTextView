87620b63fdfa149f56ccea477fbf86ce367fd8f6
杨三宁 <ysn_2020@163.com>
2019-04-22 14:24:27 +0800

    [ +] 增加MagicalTextView

---
+++ app/src/main/java/ysn/com/magicaltextview/MainActivity.java
@@ -0,0 +1,19 @@
+package ysn.com.magicaltextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String text="宋茜：1987年2月2日出生于山东省青岛市，中国内地女演员、歌手，亚洲多栖发展女艺人。" +
+                "2008年，宋茜作为演员、广告模特在亚洲地区正式开始演艺活动。2009年，担任女子流行演唱组合f(x)队长，以歌手身份正式出道。";
+
+        MagicalTextView autoWrapTextView = findViewById(R.id.main_activity_text_view);
+        autoWrapTextView.setText(text);
+    }
+}
---
+++ magicaltextview/src/main/java/ysn/com/magicaltextview/MagicalTextView.java
@@ -0,0 +1,256 @@
+package ysn.com.magicaltextview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Build;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author yangsanning
+ * @ClassName MagicalTextView
+ * @Description 一句话概括作用
+ * @Date 2019/4/22
+ * @History 2019/4/22 author: description:
+ */
+public class MagicalTextView extends View {
+
+    private TextPaint textPaint;
+    private String defaultText, detailsText;
+    private int textColor, detailsTextColor;
+    private int textSize;
+    private int detailsMarginLeft;
+
+    private int paddingLeft, paddingRight, paddingTop, paddingBottom;
+    /**
+     * 行宽
+     */
+    private int rowWidth;
+    /**
+     * 最大显示行数
+     */
+    private int maxLine;
+
+    private int viewWidth, viewHeight;
+
+    private char[] textCharArray;
+    private List<String> textList;
+    private Rect[] textRectArray = null;
+    private String endTagText = "...";
+    private int endTagTextWidth, detailsTextWidth, lineTextWidth;
+
+    public MagicalTextView(Context context) {
+        this(context, null);
+    }
+
+    public MagicalTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MagicalTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MagicalTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        initAttrs(context, attrs);
+        initPaint();
+        initView();
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MagicalTextView);
+        defaultText = typedArray.getString(R.styleable.MagicalTextView_mtv_default_text);
+        textColor = typedArray.getColor(R.styleable.MagicalTextView_mtv_default_text_color, Color.BLACK);
+        detailsText = typedArray.getString(R.styleable.MagicalTextView_mtv_details_text);
+        detailsTextColor = typedArray.getColor(R.styleable.MagicalTextView_mtv_details_text_color, Color.BLUE);
+        detailsMarginLeft = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_details_margin_left, 0);
+        textSize = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_text_size, 50);
+
+        paddingLeft = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_padding_left, 0);
+        paddingRight = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_padding_right, 0);
+        paddingTop = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_padding_top, 0);
+        paddingBottom = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_padding_bottom, 0);
+
+        rowWidth = typedArray.getDimensionPixelSize(R.styleable.MagicalTextView_mtv_row_width, 7);
+        maxLine = typedArray.getInteger(R.styleable.MagicalTextView_mtv_max_line, 3);
+
+        typedArray.recycle();
+    }
+
+    private void initPaint() {
+        textPaint = new TextPaint();
+        textPaint.setAntiAlias(true);
+        textPaint.setTextSize(textSize);
+        textPaint.setColor(textColor);
+        textPaint.setTextAlign(Paint.Align.LEFT);
+    }
+
+    private void initView() {
+        setText(defaultText);
+
+        endTagTextWidth = 0;
+        endTagText = endTagText == null ? "" : endTagText;
+        for (char c : endTagText.toCharArray()) {
+            endTagTextWidth += getSingleCharWidth(c);
+        }
+
+        detailsTextWidth = 0;
+        detailsText = detailsText == null ? "" : detailsText;
+        for (char c : detailsText.toCharArray()) {
+            detailsTextWidth += getSingleCharWidth(c);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        initText(MeasureSpec.getMode(heightMeasureSpec));
+    }
+
+    /**
+     * 初始化文本
+     */
+    private void initText(int heightMode) {
+        if (textCharArray == null) {
+            return;
+        }
+
+        textList = new ArrayList<>();
+        lineTextWidth = getMeasuredWidth() - paddingLeft - paddingRight;
+        int currentLTextWidth = 0;
+        StringBuilder textStringBuilder = new StringBuilder();
+        for (int i = 0, length = textCharArray.length; i < length; i++) {
+            char textChar = textCharArray[i];
+            currentLTextWidth += getSingleCharWidth(textChar);
+            if (currentLTextWidth > lineTextWidth) {
+                textList.add(textStringBuilder.toString());
+                textStringBuilder.delete(0, textStringBuilder.length());
+                currentLTextWidth = 0;
+                i--;
+            } else {
+                textStringBuilder.append(textChar);
+                if (i == length - 1) {
+                    textList.add(textStringBuilder.toString());
+                }
+            }
+        }
+
+        int textHeight = 0;
+        textRectArray = new Rect[textList.size()];
+        for (int i = 0, length = textList.size(); i < length && i < maxLine; i++) {
+            String lineText = textList.get(i);
+            Rect lineTextRect = new Rect();
+            textPaint.getTextBounds(lineText, 0, lineText.length(), lineTextRect);
+            if (heightMode == MeasureSpec.AT_MOST) {
+                if (i == length - 1 || i == maxLine - 1) {
+                    textHeight += lineTextRect.height() + paddingBottom + paddingTop;
+                }else {
+                    textHeight += (lineTextRect.height() + rowWidth);
+                }
+            } else {
+                if (textHeight == 0) {
+                    textHeight = getMeasuredHeight();
+                }
+            }
+            textRectArray[i] = lineTextRect;
+        }
+
+        setMeasuredDimension(getMeasuredWidth(), textHeight);
+    }
+
+    /**
+     * 得到单个char的宽度
+     */
+    public float getSingleCharWidth(char textChar) {
+        float[] width = new float[1];
+        textPaint.getTextWidths(new char[]{textChar}, 0, 1, width);
+        return width[0];
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        drawText(canvas);
+    }
+
+    /**
+     * 绘制文本
+     */
+    public void drawText(Canvas canvas) {
+        if (textList == null || textList.size() == 0) {
+            return;
+        }
+
+        textPaint.setColor(textColor);
+        int marginTop = getTopTextMarginTop();
+        for (int i = 0, length = textList.size(); i < length; i++) {
+            String lineText = textList.get(i);
+
+            if (maxLine == i + 1) {
+                canvas.drawText(getPassText(lineText), paddingLeft, marginTop, textPaint);
+                textPaint.setColor(detailsTextColor);
+                canvas.drawText(detailsText, (viewWidth - paddingRight - detailsTextWidth), marginTop, textPaint);
+                break;
+            } else {
+                canvas.drawText(lineText, paddingLeft, marginTop, textPaint);
+                marginTop += (textRectArray[i].height() + rowWidth);
+            }
+        }
+    }
+
+    private int getTopTextMarginTop() {
+        return textRectArray[0].height() / 2 + paddingTop + getFontSpace();
+    }
+
+    private int getFontSpace() {
+        Paint.FontMetricsInt fontMetrics = textPaint.getFontMetricsInt();
+        return (fontMetrics.descent - fontMetrics.ascent) / 2 - fontMetrics.descent;
+    }
+
+    private String getPassText(String text) {
+        lineTextWidth = (int) textPaint.measureText(text);
+        int fixWidth = viewWidth - paddingLeft - paddingRight - endTagTextWidth - detailsMarginLeft - detailsTextWidth;
+        if (lineTextWidth >= fixWidth) {
+            return getPassText(text.substring(0, text.length() - 1));
+        }
+        return text + endTagText;
+    }
+
+    /**
+     * 设置文本
+     */
+    public void setText(String text) {
+        if (TextUtils.isEmpty(text)) {
+            return;
+        }
+
+        textCharArray = text.toCharArray();
+        requestLayout();
+    }
+}
